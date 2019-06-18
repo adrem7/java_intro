aea02240885b9140bfc3743571f159f0956b6142
adrem7 <adrian.raisin@gmail.com>
2019-06-18 15:25:05 +0100

    Gradle built

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -1,10 +1,27 @@
 // what imports will you need?
+import java.util.Scanner;
 
+// import javax.lang.model.util.ElementScanner6;
 // can you remember how to set up your main function?
-
+public class ControlFlow {
+  public static void main(String[] args) {
 // find a way to read in user input
-
+    int inputNumber = 0;
+    while (inputNumber != 17) {
+      System.out.println("Please provide a number: ");
+      Scanner in = new Scanner(System.in);
+      inputNumber = in.nextInt();
 // write a method that will check if a number is odd or even
 // (assume user only ever enters integers)
+      boolean isEven = ((inputNumber % 2) == 0);
 
 // print the answer to the console
+      if (isEven) {
+        System.out.println(inputNumber + " is even");
+      }
+      else {
+        System.out.println(inputNumber + " is odd");
+      }
+    }
+  }
+}
--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,11 @@
 // can you remember how to set up your main function?
-
+public class LoopThroughArray {
+  public static void main(String[] args) {
 // loop through an integer array and print out each element
+    int[] array_of_integers = {1,2,3,4,5};
+    
+    for (int i = 0; i < 5; i++) {
+      System.out.println(array_of_integers[i]);
+    }
+  }
+}
